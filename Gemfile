b1db9a3d063dc46f270dccfaa2e5a516b5ba7f74
hmu332233 <hmu332233@naver.com>
2017-02-11 19:49:56 +0000

    fcm 메세지 보내기 구현 완료

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'fcm'
 
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.5'
--- app/controllers/fcm_controller.rb
+++ app/controllers/fcm_controller.rb
@@ -1,13 +1,22 @@
-require 'FCM'
 
 class FcmController < ApplicationController
   
+  # def sendMessage
+    
+  #   fcm = FCMb.new
+    
+  #   puts "test"
+  #   fcm.sendMessage2
+    
+  # end
+  
   def sendMessage
-    
-    fcm = FCM.new
-    
-    fcm.sendMessage("test")
-    
+      fcm = FCM.new(ENV['FCM_KEY'])
+        
+      response = fcm.send_with_notification_key("/topics/news",
+            data: {message: "테스트입니다 근데 왜케 늦게와?",title: "이건 타이틀이얌!"})
+        
+      puts response
   end
   
 end
